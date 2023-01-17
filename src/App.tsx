50e0fdb741ca671cc79cf4030185e3eb256193ec
ShaieneKun <rafaelshaiene115@gmail.com>
2023-01-17 04:17:35 +0000

    Started project

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,22 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios'
+
+// import fetch from 'node-fetch';
+
+
+async function makeReq(){
+  const response = await axios("http://localhost:3000");
+  console.log(response.status);
+}
 
 function App() {
+  // const response = await axios("http://localhost:3000");
+
+  // console.log(response);
+  makeReq()
+
   return (
     <div className="App">
       <header className="App-header">
