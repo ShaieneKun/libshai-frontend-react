faca46924ca9943f94624fb5ff5a0859bcae5fde
ShaieneKun <rafaelshaiene115@gmail.com>
2023-01-21 20:25:22 +0000

    Added api request and displayed on page

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,59 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import axios from 'axios'
 
-// import fetch from 'node-fetch';
+type articleJson = {
+  id: number,
+  title: string,
+  summary: string,
+  body: string,
+  date_of_creation: string,
+  date_of_last_update: string,
+  author: number,
+  tags: number[],
+}
 
+type ArticleArrayJson = articleJson[]
 
-async function makeReq(){
-  const response = await axios("http://localhost:3000");
-  console.log(response.status);
+async function makeReq(success: Function, fail: Function){
+  try {
+    const response: Response = await fetch("http://localhost:8000/blog//api/");
+    const responseJson: ArticleArrayJson = await response.json();
+
+    console.log(responseJson);
+
+    success(responseJson)
+  } catch (error) {
+    fail(error)
+  }
 }
 
 function App() {
-  // const response = await axios("http://localhost:3000");
 
-  // console.log(response);
-  makeReq()
+  const [articlesText, setArticlesText] = React.useState<ArticleArrayJson | []>([])
+
+  React.useEffect(() => {
+    makeReq(
+      (responseJson: ArticleArrayJson) => setArticlesText(responseJson),
+      (error: Error) => console.log(error)
+    )
+  }, [])
 
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Articles</h1>
+        <div className="posts-container">
+          {articlesText.map((article) => {
+            return (
+              <div className="article-card" key={article.id}>
+                <h2 className="article-title">{article.title}</h2>
+                <p className="article-body">{article.summary}</p>
+              </div>
+            );
+          })}
+        </div>
       </header>
     </div>
   );
